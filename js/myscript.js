aa7e2b4a6bcb6c664257f67f8ed25cab119d437e
Giuseppe Rotolo <rotolo37@gmail.com>
2020-05-21 10:56:02 +0200

    general fix

--- js/myscript.js
+++ js/myscript.js
@@ -1,6 +1,5 @@
 // domande utente
 var numChilometri = parseInt( prompt('Quanti chilometri vuoi percorrere?' ) ) ;
-
 var etaUtente = parseInt (prompt('Quanti anni hai?'));
 
 // prezzo biglietto
@@ -8,23 +7,24 @@ var bigliettoNormale = numChilometri * 0.21;
 
 // scontistiche biglietto
 var scontoQua = bigliettoNormale * 0.4;
-
 var scontoVen = bigliettoNormale * 0.2;
+var bigliettoScontatoVen = (bigliettoNormale) - (scontoVen);
+var bigliettoScontatoQua = (bigliettoNormale) - (scontoQua);
+var messaggio;
 
-var bigliettoScontatoVen = bigliettoNormale - scontoVen;
-
-var bigliettoScontatoQua = bigliettoNormale - scontoQua;
-
-
-
-if ( !isNaN(numChilometri) && !isNaN(etaUtente) ) {
-    if ( etaUtente < 18 ) {
-        document.getElementById('prezzo_finale').innerHTML = 'Il costo del biglietto è di € ' + bigliettoScontatoVen ;
-    } else if ( etaUtente > 65 ) {
-        document.getElementById('prezzo_finale').innerHTML = 'Il costo del biglietto è di € ' + bigliettoScontatoQua ;
-    } else {
-        document.getElementById('prezzo_finale').innerHTML = 'Il costo del biglietto è di € ' + bigliettoNormale ;
-    }
-} else {
-    alert('immetti un numero per calcolare il tuo biglietto!')
+// gestione errore
+if ( isNaN(numChilometri) || isNaN(etaUtente) ) {
+    messaggio = 'Errore, ricontrolla i dati immessi'
 }
+// calcolo biglietto se non ci sono errori
+else {
+  if (etaUtente < 18 ) {
+    messaggio = 'Il tuo prezzo e ' + bigliettoScontatoVen.toFixed(2);
+
+  }
+ else if (etaUtente > 65 ) {
+  messaggio = 'Il tuo prezzo e ' + bigliettoScontatoQua.toFixed(2);
+  }
+}
+// stampo il prezzo
+document.getElementById('prezzo_finale').innerHTML = messaggio;
